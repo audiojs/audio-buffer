f45e67856fb029e735d049b77115a27e4a8e17e9
Dmitry <df.creative@gmail.com>
2015-12-29 21:46:59 -0500

    Merge browser/node code into a single file

--- index.js
+++ index.js
@@ -1,1 +1,183 @@
-module.exports = require('./buffer.js')
+/**
+ * AudioBuffer class
+ *
+ * @module audio-buffer/buffer
+ */
+
+
+var isBuffer = require('is-buffer');
+var b2ab = require('buffer-to-arraybuffer');
+var isBrowser = require('is-browser');
+var isAudioBuffer = require('is-audio-buffer');
+var context = require('audio-context');
+
+
+/**
+ * @constructor
+ *
+ * @param {∀} data Any collection-like object
+ */
+function AudioBuffer (channels, data, sampleRate) {
+	if (!(this instanceof AudioBuffer)) return new AudioBuffer(channels, data, sampleRate);
+
+	//if one argument only - it is surely data or length
+	//having new AudioBuffer(2) does not make sense as 2 - number of channels
+	if (data == null) {
+		data = channels;
+		channels = null;
+	}
+	//audioCtx.createBuffer() - complacent arguments
+	else {
+		if (sampleRate != null) this.sampleRate = sampleRate;
+		if (channels != null) this.numberOfChannels = channels;
+	}
+
+
+	//if number = create new array (spec's case - suppose that most widespread)
+	if (typeof data === 'number') {
+		this.data = [];
+		for (var i = 0; i < this.numberOfChannels; i++ ) {
+			this.data.push(new AudioBuffer.FloatArray(data));
+		}
+	}
+	//if other audio buffer passed - create fast clone of it
+	//if WAA AudioBuffer - get buffer’s data (it is bounded)
+	else if (isAudioBuffer(data)) {
+		this.data = [];
+		if (channels == null) this.numberOfChannels = data.numberOfChannels;
+		if (sampleRate == null) this.sampleRate = data.sampleRate;
+
+		//copy channel's data
+		for (var i = 0, l = data.numberOfChannels; i < l; i++) {
+			this.data.push(data.getChannelData(i).slice());
+		}
+	}
+	//TypedArray, Buffer, DataView etc, or ArrayBuffer
+	//NOTE: node 4.x+ detects Buffer as ArrayBuffer view
+	else if (ArrayBuffer.isView(data) || data instanceof ArrayBuffer || isBuffer(data)) {
+		this.data = [];
+		if (isBuffer(data)) {
+			data = b2ab(data);
+		}
+		if (!(data instanceof AudioBuffer.FloatArray)) {
+			data = new AudioBuffer.FloatArray(data.buffer || data);
+		}
+		var len = data.length / this.numberOfChannels;
+		for (var i = 0; i < this.numberOfChannels; i++ ) {
+			//NOTE: we could’ve done subarray here to create a reference, but...
+			//it will not be compatible with the WAA buffer - it cannot be a reference
+			this.data.push(data.slice(i* len, i * len + len));
+		}
+	}
+	//if array - parse channeled data
+	else if (Array.isArray(data)) {
+		this.data = [];
+		//if separated data passed already
+		if (data[0] instanceof Object) {
+			for (var i = 0; i < this.numberOfChannels; i++ ) {
+				this.data.push(new AudioBuffer.FloatArray(data[i]));
+			}
+		}
+		//plain array passed
+		else {
+			var len = Math.floor(data.length / this.numberOfChannels);
+			for (var i = 0; i < this.numberOfChannels; i++ ) {
+				var channelData = data.slice(i * len, i * len + len);
+				//force channel data be numeric
+				if (channelData[0] == null) channelData = len;
+				this.data.push(new AudioBuffer.FloatArray(channelData));
+			}
+		}
+	}
+	//if ndarray, typedarray or other data-holder passed - redirect plain databuffer
+	else if (data.data || data.buffer) {
+		return new AudioBuffer(this.numberOfChannels, data.data || data.buffer, this.sampleRate);
+	}
+	//if none passed (radical weird case), or no type detected
+	else {
+		//it’d be strange use-case
+		throw Error('Failed to create buffer: check provided arguments');
+	}
+
+
+	//set up params
+	this.length = this.data[0].length;
+	this.duration = this.length / this.sampleRate;
+
+
+	//for browser - just return WAA buffer
+	if (isBrowser) {
+		//create WAA buffer
+		var audioBuffer = AudioBuffer.context.createBuffer(this.numberOfChannels, this.length, this.sampleRate);
+
+		//fill channels
+		for (var i = 0; i < this.numberOfChannels; i++) {
+			audioBuffer.getChannelData(i).set(this.getChannelData(i));
+		}
+
+		return audioBuffer;
+	}
+};
+
+
+/**
+ * Default params
+ */
+AudioBuffer.prototype.numberOfChannels = 2;
+AudioBuffer.prototype.sampleRate = 44100;
+
+
+/** Type of storage to use */
+AudioBuffer.FloatArray = Float32Array;
+
+
+/** Create audio buffer from any argument */
+AudioBuffer.from = function (arg) {
+
+};
+
+
+/** Set context, though can be redefined */
+AudioBuffer.context = context;
+
+
+/**
+ * Return data associated with the channel.
+ *
+ * @return {Array} Array containing the data
+ */
+AudioBuffer.prototype.getChannelData = function (channel) {
+	//FIXME: ponder on this, whether we really need that rigorous check, it may affect performance
+	if (channel > this.numberOfChannels || channel < 0 || channel == null) throw Error('Cannot getChannelData: channel number (' + channel + ') exceeds number of channels (' + this.numberOfChannels + ')');
+	return this.data[channel];
+};
+
+
+/**
+ * Place data to the destination buffer, starting from the position
+ */
+AudioBuffer.prototype.copyFromChannel = function (destination, channelNumber, startInChannel) {
+	var data = this.data[channelNumber];
+	if (startInChannel == null) startInChannel = 0;
+	for (var i = startInChannel, j = 0; i < data.length && j < destination.length; i++, j++) {
+		destination[j] = data[i];
+	}
+};
+
+
+/**
+ * Place data from the source to the channel, starting (in self) from the position
+ * Clone of WAAudioBuffer
+ */
+AudioBuffer.prototype.copyToChannel = function (source, channelNumber, startInChannel) {
+	var data = this.data[channelNumber];
+
+	if (!startInChannel) startInChannel = 0;
+
+	for (var i = startInChannel, j = 0; i < this.length && j < source.length; i++, j++) {
+		data[i] = source[j];
+	}
+};
+
+
+module.exports = AudioBuffer;
