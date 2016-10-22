cdc59ff35e59a86686d11771b95b59d3f201cb20
Connor Hartley <vectrixu+gh@gmail.com>
2016-10-22 13:32:41 +1300

    Channel length fix
    Allows the maximum channel length to be met.

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ AudioBuffer.prototype.sampleRate = AudioBuffer.context.sampleRate || 44100;
  */
 AudioBuffer.prototype.getChannelData = function (channel) {
 	//FIXME: ponder on this, whether we really need that rigorous check, it may affect performance
-	if (channel >= this.numberOfChannels || channel < 0 || channel == null) throw Error('Cannot getChannelData: channel number (' + channel + ') exceeds number of channels (' + this.numberOfChannels + ')');
+	if (channel > this.numberOfChannels || channel < 0 || channel == null) throw Error('Cannot getChannelData: channel number (' + channel + ') exceeds number of channels (' + this.numberOfChannels + ')');
 
 	return this.data.subarray(channel * this.length, (channel + 1) * this.length);
 };
